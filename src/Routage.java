1d4552c94088db4505daaba2626b6b0eececab18
pierrebergesupelec <pierrebergesupelec@gmail.fr>
2014-10-14 23:07:35 +0200

    Update Routage.java

--- src/Routage.java
+++ src/Routage.java
@@ -7,14 +7,14 @@ public class Routage {
 	ArrayList<Integer> route ; 
 	double distance ;
 	
-	//Crée une route aléatoire
+	//CrÃ©e une route alÃ©atoire
 	public Routage(Graphe g) {
 		this.g = g;
 		routeInitiale();
 		distance = newDistance();
 	}
 	
-	//Route définie par une liste d'indices
+	//Route dÃ©finie par une liste d'indices
 	public Routage(Graphe g, ArrayList<Integer> liste, double distance){
 		this.g=g;
 		this.route=liste;
@@ -49,19 +49,19 @@ public class Routage {
 		return route.size();
 	}
     
- // Renvoie une route initiale aléatoire
+ // Renvoie une route initiale alÃ©atoire
     public void routeInitiale() {
     	int n = g.nombreDeNoeuds();
     	ArrayList<Integer> liste = new ArrayList<Integer>();
         for (int index = 0; index < n; index++) {
           liste.add(new Integer(index));
         }
-        // rÃ©organise alÃ©atoirement l'ordre de visite
+        // rÃƒÂ©organise alÃƒÂ©atoirement l'ordre de visite
         Collections.shuffle(liste);
         route = liste;
     }
     
-    //Modifie la route de manière aléatoire
+    //Modifie la route de maniÃ¨re alÃ©atoire
     public void routeAleatoire() {
     	Collections.shuffle(route);
     }
@@ -89,14 +89,33 @@ public class Routage {
     	Collections.swap(route,i,j);
     }
     
-    //Effectue une mutation et modifie la distance
+   //Effectue une mutation et modifie la distance
     public void twoPointsMove() {
     	int n = tailleRoute();
     	 // On choisit deux positions du parcours au hasard.
         int randIndex1 = (int) (tailleRoute() * Math.random());
         int randIndex2 = (int) (tailleRoute() * Math.random());
         swap(randIndex1,randIndex2);
-        //Repertorie les arêtes à rajouter et à supprimer pendant la mutation puis modifie distance
+        //Repertorie les arÃªtes Ã  rajouter et Ã  supprimer pendant la mutation puis modifie distance
+        if (Math.abs(randIndex1 - randIndex2) == 1) {
+        	int indexMin = Math.min(randIndex1,randIndex2);
+        	int indexMax = Math.max(randIndex1,randIndex2);
+        	double plus1 = g.longueurEntre(route.get(indexMin),route.get(getPreviousIndex(indexMin,n)));
+            double plus2 = g.longueurEntre(route.get(indexMax),route.get(getNextIndex(indexMax,n)));
+            double moins1 = g.longueurEntre(route.get(indexMin),route.get(getNextIndex(indexMax,n)));
+            double moins2 = g.longueurEntre(route.get(indexMax),route.get(getPreviousIndex(indexMin,n)));
+            distance += (plus1 + plus2) - (moins1 + moins2);
+        } else {
+        	if (Math.abs(randIndex1 - randIndex2) == (n - 1)) {
+        		int indexMin = Math.min(randIndex1,randIndex2);
+            	int indexMax = Math.max(randIndex1,randIndex2);
+            	double plus1 = g.longueurEntre(route.get(indexMin),route.get(getNextIndex(indexMin,n)));
+                double plus2 = g.longueurEntre(route.get(indexMax),route.get(getPreviousIndex(indexMax,n)));
+                double moins1 = g.longueurEntre(route.get(indexMin),route.get(getPreviousIndex(indexMax,n)));
+                double moins2 = g.longueurEntre(route.get(indexMax),route.get(getNextIndex(indexMin,n)));
+                distance += (plus1 + plus2) - (moins1 + moins2);
+        	} else {
+        
         double plus1 = g.longueurEntre(route.get(randIndex1),route.get(getPreviousIndex(randIndex1,n)));
         double plus2 = g.longueurEntre(route.get(randIndex1),route.get(getNextIndex(randIndex1,n)));
         double plus3 = g.longueurEntre(route.get(randIndex2),route.get(getPreviousIndex(randIndex2,n)));
@@ -106,6 +125,8 @@ public class Routage {
         double moins3 = g.longueurEntre(route.get(randIndex2),route.get(getPreviousIndex(randIndex1,n)));
         double moins4 = g.longueurEntre(route.get(randIndex2),route.get(getNextIndex(randIndex1,n)));
         distance += (plus1 + plus2 + plus3 + plus4) - (moins1 + moins2 + moins3 +moins4);
+        	}
+        }
     }
     
     //Calcule la distance d'une route
